088a74d3676304e6e33615d0119f53fa138d3ac8
techehub <vijeesh.tp@techehub.com>
2020-01-10 13:27:31 +0530

    inheritance

--- class1.py
+++ class1.py
@@ -1,4 +1,4 @@
-class Student :
+class Student (object) :
 
     def __init__ (self, name, rollno, branch, m1, m2, m3):
         self.name = name
@@ -8,13 +8,11 @@ class Student :
         self.mark2 = m2
         self.mark3 = m3
 
-
     def total (self):
         return self.mark1 + self.mark2 + self.mark3
 
     def avg (self):
         return self.total()/3
-
 
     def display (self):
         print ("--------------------")
@@ -25,10 +23,20 @@ class Student :
         print("Average :", self.avg())
 
 
+class FulltimeStudent (Student ):
 
-s1 = Student("Kabeer", "123", "CS", 22, 33, 44)
-s2 = Student("Seetha", "222", "CS", 25, 35, 45)
-s3 = Student("Tom", "333", "CS", 26, 36, 46)
+    def __init__(self, name, rollno, branch, m1, m2, m3, hours):
+        super().__init__(name, rollno, branch, m1, m2, m3)
+        self.hours = hours
+
+    def display (self):
+        super().display()
+        print ("Hour :", self.hours)
+
+
+s1 = FulltimeStudent("Kabeer", "123", "CS", 22, 33, 44, 5)
+s2 = FulltimeStudent("Seetha", "222", "CS", 25, 35, 45, 6)
+s3 = FulltimeStudent("Tom", "333", "CS", 26, 36, 46, 7)
 
 s1.display()
 s2.display()
